dafee78b6d0b7530423e2277703eab10f6270f91
wonwoohan <cooldnjsdn@gmail.com>
2024-06-06 15:38:47 +0900

    update test code

--- test/main.dart
+++ test/main.dart
@@ -7,7 +7,7 @@ import 'package:size_tailored_text/size_tailored_text.dart';
 void main() {
   test('Sample test', () {
     final String text = List.generate(Random().nextInt(500), (index) => index).toString();
-    final TextStyle textStyle = TextStyle(
+    const TextStyle textStyle = TextStyle(
       fontSize: 10,
     );
     expect(SizeTailoredText(text: text, textStyle: textStyle, maxLines: Random().nextInt(10),), 'expected output');
