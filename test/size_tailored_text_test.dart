124e2b59a6446bc5f2f0dc8136742ceafd1fcf24
wonwoohan <cooldnjsdn@gmail.com>
2024-06-06 16:24:44 +0900

    update widget test code

--- test/size_tailored_text_test.dart
+++ test/size_tailored_text_test.dart
@@ -5,28 +5,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:size_tailored_text/size_tailored_text.dart';
 
 void main() {
-  test('Sample test', () {
-    final String text =
-        List.generate(Random().nextInt(500), (index) => index).toString();
-    const TextStyle textStyle = TextStyle(
-      fontSize: 10,
-    );
-    expect(
-        SizeTailoredText(
-          text: text,
-          textStyle: textStyle,
-          maxLines: Random().nextInt(10),
-        ),
-        'expected output');
-  });
-
   testWidgets(
     'Widget Test',
     (WidgetTester tester) async {
-      final String text =
-          List.generate(Random().nextInt(500), (index) => index).toString();
+      final String text = List.generate(Random().nextInt(200), (index) => index).toString();
       const TextStyle textStyle = TextStyle(
-        fontSize: 10,
+        fontSize: 30,
       );
       await tester.pumpWidget(
         MaterialApp(
@@ -40,7 +24,6 @@ void main() {
         ),
       );
       expect(find.byType(SizeTailoredText), findsOneWidget);
-      expect(find.text(text), findsOneWidget);
     },
   );
 }
